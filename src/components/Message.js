5789c19cecb7407c9cbc96ecf53328ad11b6b30c
carter <wilsoncr2@g.cofc.edu>
2023-10-13 15:43:31 -0400

    saving

--- src/components/Message.js
+++ src/components/Message.js
@@ -2,7 +2,14 @@ import React from 'react'
 import "../styling/DirectMessage.css"
 const Message = () => {
   return (
-    <div>Message</div>
+    <div className="message">
+      <div className="messageInfo">
+        <img src="https://images.pexels.com/photos/18713194/pexels-photo-18713194/free-photo-of-sea-city-water-street.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load" 
+        alt =""
+        />
+        <span>just now</span>
+      </div>
+    </div>
   )
 }
 export default Message;
