47021ba24da22dac2f73dbf381d34c97a08e31de
carter <wilsoncr2@g.cofc.edu>
2023-10-13 17:02:51 -0400

    fixed colors on dm page

--- src/components/Message.js
+++ src/components/Message.js
@@ -2,12 +2,18 @@ import React from 'react'
 import "../styling/DirectMessage.css"
 const Message = () => {
   return (
-    <div className="message">
+    <div className="message owner">
       <div className="messageInfo">
         <img src="https://images.pexels.com/photos/18713194/pexels-photo-18713194/free-photo-of-sea-city-water-street.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load" 
         alt =""
         />
         <span>just now</span>
+      </div>
+      <div className="messageContent">
+        <p>hello</p>
+        <img src="https://images.pexels.com/photos/18713194/pexels-photo-18713194/free-photo-of-sea-city-water-street.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load" 
+        alt=""
+        />
       </div>
     </div>
   )
