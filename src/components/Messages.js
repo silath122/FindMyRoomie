972b091b58084cddae7041929534b6f847f93399
Carter Wilson <wilsoncr2@g.cofc.edu>
2023-10-19 11:19:22 -0400

    made chat fit screen and scrolls to bottom

--- src/components/Messages.js
+++ src/components/Messages.js
@@ -1,16 +1,22 @@
 import React from 'react'
 import Message from "./Message"
 import {ChatContext} from  "../context/ChatContext"
-import {useState, useContext, useEffect} from "react";
+import {useState, useContext, useEffect, useRef} from "react";
 import {collection, query, where, getDocs, serverTimestamp, doc, setDoc, updateDoc, onSnapshot} from "firebase/firestore";
 import {firestore} from "../firebase";
 const Messages = () => {
   const [messages,setMessages] = useState([])
   const {data} = useContext(ChatContext);
-
+  const messagesDivRef = useRef(null);
+  const scrollToBottom = () => {
+    if (messagesDivRef.current) {
+      messagesDivRef.current.scrollTop = messagesDivRef.current.scrollHeight;
+    }
+  };
   useEffect(()=>{
     const unSub = onSnapshot(doc(firestore, "chats", data.chatId), (doc)=>{
       doc.exists() && setMessages(doc.data().messages)
+      scrollToBottom();
     })
 
     return()=>{
@@ -18,7 +24,7 @@ const Messages = () => {
     }
   })
   return (
-      <div className="messages">
+      <div className="messages" ref={messagesDivRef}>
         <Message/>
         <Message/>
         <Message/>
