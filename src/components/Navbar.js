6daa73e314fa1901b1eeda6d9f01b4cb254f19ce
Carter Wilson <wilsoncr2@g.cofc.edu>
2023-10-20 16:29:29 -0400

    fixed navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,34 +1,71 @@
+// import "../styling/Navbar.css";
+// import {Link } from "react-router-dom";
+// import {logout} from "../firebase"
+// import Button from "react-bootstrap/Button";
+// import Typography from "@mui/material/Typography";
+// export default function Navbar(){
+
+//     return (
+    
+//             <nav className="nav">
+//                 <a href= "/home">
+
+//                     <img className="logo"
+//                          src={require("../pictures/FindMyRoomieLogo.png")}
+//                          alt="FindMyRoomie"/>
+//                 </a>
+//                 <Typography
+//                     className="title"
+//                     sx={{letterSpacing: '3px', fontSize: '35px', fontWeight: 'bold', fontFamily: "Segoe UI Emoji"}}>
+//                     Find My Roomie
+//                 </Typography>
+//                 <ul>
+//                     <button onClick={()=>logout()}>Logout</button>
+//                     <Link to="/settings">
+//                         <Typography sx={{align: 'center', paddingTop:'5px'}}> Settings</Typography>
+
+//                     </Link>
+            
+//                 </ul>
+
+//             </nav>
+        
+//    )
+// }
+
 import "../styling/Navbar.css";
-import {Link } from "react-router-dom";
-import {logout} from "../firebase"
+import { Link } from "react-router-dom";
+import { logout } from "../firebase";
 import Button from "react-bootstrap/Button";
 import Typography from "@mui/material/Typography";
-export default function Navbar(){
 
-    return (
-    
-            <nav className="nav">
-                <a href= "/home"
-                   sx = {{paddingTop:'5px'}}>
-                    <img sx={{paddingTop:'-5px'}}
-                         src={require("../pictures/FindMyRoomieLogo.png")}
-                         alt="FindMyRoomie" class="logo"/>
-                </a>
-                <Typography
-                    className="title"
-                    sx={{letterSpacing: '3px', fontSize: '35px', fontWeight: 'bold', fontFamily: "Segoe UI Emoji"}}>
-                    Find My Roomie
-                </Typography>
-                <ul>
-                    <button onClick={()=>logout()}>Logout</button>
-                    <Link to="/settings">
-                        <Typography sx={{align: 'center', paddingTop:'5px'}}> Settings</Typography>
-
-                    </Link>
-            
-                </ul>
-
-            </nav>
-        
-    )
+export default function Navbar() {
+  return (
+    <nav className="nav">
+      <Link to="/home" className="logo-link">
+        <img
+          className="logo"
+          src={require("../pictures/FindMyRoomieLogo.png")}
+          alt="FindMyRoomie"
+        />
+      </Link>
+      <Typography
+        className="title"
+        sx={{
+          letterSpacing: "3px",
+          fontSize: "35px",
+          fontWeight: "bold",
+          fontFamily: "Segoe UI Emoji",
+        }}
+      >
+        Find My Roomie
+      </Typography>
+      <ul>
+        <button onClick={() => logout()}>Logout</button>
+        <Link to="/settings">
+          <Typography sx={{ align: "center", paddingTop: "5px" }}> Settings</Typography>
+        </Link>
+      </ul>
+    </nav>
+  );
 }
