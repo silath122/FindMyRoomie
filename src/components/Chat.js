ee8f3469aa6e15b616859f1a06bb0f883e09b131
Carter Wilson <wilsoncr2@g.cofc.edu>
2023-11-09 13:08:52 -0500

    chat title matching name clicked

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -6,16 +6,28 @@ import {ChatContext} from "../context/ChatContext";
 import {useContext} from "react";
 const Chat = () =>{
     const {data} = useContext(ChatContext);
+    console.log("data")
     console.log(data)
-    return(
-        <div className="chat">
-            <div className="chatInfo">
-                <span>{JSON.parse(data.user?.displayName)}</span>
+    if(data.chatId != 'null'){
+        return(
+            <div className="chat">
+                <div className="chatInfo">
+                    <span>{JSON.parse(data.user?.displayName)}</span>  
+                </div>
+                <Messages/>
+                <Input/>
             </div>
-            <Messages/>
-            <Input/>
-        </div>
-        
-    )
+        )
+    }else{
+        return(
+            <div className="chat">
+                <div className="chatInfo">
+                </div>
+                <Messages/>
+                <Input/>
+            </div>
+            
+        )
+    }
 }
 export default Chat
