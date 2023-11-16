483f61407f420f99cc7985f2492e21d819e92b4b
Siah Thomas <thomassl1@g.cofc.edu>
2023-11-16 18:26:03 -0500

    modified:   src/firebase.js modified:   src/pages/SurveyForm.js
    Surveys data: now saves document id as uid & has uid field
    Users data: now saves document id as uid & has uid field

--- src/pages/SurveyForm.js
+++ src/pages/SurveyForm.js
@@ -230,21 +230,14 @@ function SurveyForm() {
 
     const handleSurveyComplete = async () => {
         const surveyData = survey.data;
-        setFormData(surveyData);
-        setFormComplete(true);
+        setFormData(surveyData); 
+        setFormComplete(true); // it sets the form to true before it even runs.. try putting in try section - Fix later (Siah)
 
         // get the uid
         const user = auth.currentUser;
         if (user) {
             try {
                 const userId = user.uid;
-
-                // const userRef = doc(firestore, "users", userId);
-                // const userDoc = await getDoc(userRef);
-
-                
-                // const userData = userDoc.data();
-                // const userId2 = userData.uid;
 
                 storeSurveyResults(userId, surveyData);
                 navigate('/Loading');
